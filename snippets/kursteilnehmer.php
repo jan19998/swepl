72fade3c5fef64e8338432758f5ca7177e019f4e
Sebastian M <sebi.markus@googlemail.com>
2019-11-21 13:16:16 +0100

    minor clean up

---
+++ snippets/kursteilnehmer.php
@@ -0,0 +1,15 @@
+<?php
+    $semester = 'ws19/20';
+    $gruppe = 'e9';
+?>
+
+<div class="row">
+    <div id="e-mail-button-space" class="col-3">
+        button
+    </div>
+</div>
+<div class="row">
+    <?php
+
+    ?>
+</div>

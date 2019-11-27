b33be94665a046f5e122d5676947a82d72f6b909
Sebastian M <sebi.markus@googlemail.com>
2019-11-27 15:06:05 +0100

    Added Header & Footer snippets

---
+++ snippets/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <a href="impressum.php">Impressum & Datenschutz</a>
+    <a href="https://www.fh-aachen.de/" class="mittel-link">FH-Aachen</a>
+    <a href="password.php">Passwort ändern</a>
+</footer>
---
+++ snippets/header.php
@@ -0,0 +1,6 @@
+<header>
+    <img class="logo" alt="SWEpl Logo" src="images/Logo.jpg" width="50" height="50">
+    <a href="logout.php" class="logout">
+        <button type="submit" action="logout.php" value="logout">Logout</button>
+    </a>
+</header>

5016ac27b4c69cb4b86bf98a7fe16e71e12b1529
Sebastian M <sebi.markus@googlemail.com>
2019-11-28 20:28:02 +0100

    uodate betreuer

--- snippets/header.php
+++ snippets/header.php
@@ -5,7 +5,7 @@
                 <img class="logo img-fluid" alt="SWEpl Logo" src="pictures/swepl.png" width="100" height="100">
             </a>
         </div>
-        <div class="col-1 align-self-center justify-content-end">
+        <div class="col-1 align-self-center justify-content-end ">
                 <button class="btn" type="submit" action="logout.php" value="logout">Logout</button>
         </div>
     </div>

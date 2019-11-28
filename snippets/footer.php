ae1c8caf0c81cbb28d7386ff210101d113d3784b
Wandtron <Adrian.Freytag@web.de>
2019-11-28 12:24:16 +0100

    Update footer.php

--- snippets/footer.php
+++ snippets/footer.php
@@ -2,9 +2,9 @@
     <div class="row justify-content-center pt-3">
         <div class="col justify-content-center">
             <ul class="list-group list-group-horizontal justify-content-center">
-                <li class="list-group-item border-left-0 border-top-0 border-bottom-0 border-right-0">><a class="link" href="#">Impressum & Datenschutz</a></li>
-                <li class="list-group-item border-top-0 border-bottom-0 border-right-0">><a class="link" href="https://www.fh-aachen.de/" target="_blank" rel="noopener noreferrer">FH-Aachen</a></li>
-                <li class="list-group-item border-top-0 border-bottom-0 border-right-0">><a class="link" href="#">Passwort ändern</a></li>
+                <li class="list-group-item border-left-0 border-top-0 border-bottom-0 border-right-0"><a class="link" href="#">Impressum & Datenschutz</a></li>
+                <li class="list-group-item border-top-0 border-bottom-0 border-right-0"><a class="link" href="https://www.fh-aachen.de/" target="_blank" rel="noopener noreferrer">FH-Aachen</a></li>
+                <li class="list-group-item border-top-0 border-bottom-0 border-right-0"><a class="link" href="#">Passwort ändern</a></li>
             </ul>
         </div>
     </div>

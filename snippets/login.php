eb94b901a877f36abf9079b130521121453f3065
Matze1011 <58034109+Matze1011@users.noreply.github.com>
2019-12-16 18:14:51 +0100

    Update Startseite
    Session hinzugefügt nach erfolgreichem Login.

--- snippets/login.php
+++ snippets/login.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $error=''; //Um die Error message zu speichern;
 if(isset($_POST['submit'])){
     if(empty($_POST['email']) || empty($_POST['passwort'])){
@@ -14,11 +16,15 @@ if(isset($_POST['submit'])){
         $remoteConnection = mysqli_connect("127.0.0.1", "root", "", "swepl");
         //Datenbank auswählen
         $db = mysqli_select_db($remoteConnection, "swepl");
-        //sql query to fetch information of registerd user and finds user match.
+        //Datenbank nach eingegebener Email und Passwort durchsuchen
         $query = mysqli_query($remoteConnection, "SELECT * FROM swepl.benutzer WHERE `E-Mail`='$user' AND Passwort='$pass'");
 
+        //Wenn ein passender Eintrag gefunden wird ist Anzahl der rows ==1
         $rows = mysqli_num_rows($query);
         if($rows == 1){
+            //Session registrieren
+            //Wir speichern die eingegebene Email-Adresse als Session Variable in 'user' ab
+            $_SESSION['user'] = $_POST['email'];
             header("Location: jahresauswahl.php"); // Zur Betreuer Seite weitelreiten
 
             //************************************************************************
--- startseite.php
+++ startseite.php
@@ -50,7 +50,7 @@ include('snippets/login.php');
                 <input type="password" style="width:90%"  maxlength="250" name="passwort"><br>
 
                 <legend id ="legend2"></legend>
-                <input type="submit" class="btn" name="submit" value="Login">  <span><?php echo $error; ?></span>
+                <input type="submit" class="btn" name="submit" value="Login"> <span><?php echo $error; ?></span>
             </fieldset>
 
         </form>

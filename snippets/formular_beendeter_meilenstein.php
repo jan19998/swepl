a25c778654bf12f3664b5489874bb3c66c3e6fd5
shirin175 <58033806+shirin175@users.noreply.github.com>
2020-01-13 23:01:56 +0100

    Update formular_beendeter_meilenstein.php

--- snippets/formular_beendeter_meilenstein.php
+++ snippets/formular_beendeter_meilenstein.php
@@ -2,7 +2,7 @@
 <form action="meilensteincontroller.php" method="POST">
 <div class="form-group">
 <legend> Meilenstein updaten </legend>
-<select class ="form-control w-50" name ="selected_milestone">
+<select required class ="form-control w-50" name ="selected_milestone">
 <option selected>
 <label for="meilenstein_auswahlen">Meilenstein auswählen</label>
 </option>
@@ -13,7 +13,7 @@ Where Semester_FK = '$semester';";
 
 if($result = mysqli_query($remoteConnection,$query)){
     while($row = mysqli_fetch_assoc($result)){
-        echo '<option>',$row['Bezeichnung'],'</option>';
+        echo '<option >',$row['Bezeichnung'],'</option>';
     }
 }
 echo '</select>';
@@ -21,7 +21,7 @@ echo '</select>';
 </div>
 <div class="form-group">
 <label for="datum_auswahlen">Datum der Fertigstellung wählen</label>
-<input class ="form-control w-25"  type ="date" name="date_of_completion">
+<input class ="form-control w-25" type ="date" name="date_of_completion" required>
 </div>
     <input type ="submit" class ="btn border-0 btn-primary" value="Datum der Fertigstellung eintragen">
 </form>
